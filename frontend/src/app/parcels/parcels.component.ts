e1efc0c320679a5ddd1c21c4aeeeb8ca22feb992
= <antoine_mailly@hotmail.com>
2023-09-26 14:07:02 +0200

    add frontend build

--- frontend/src/app/parcels/parcels.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-parcels',
-  templateUrl: './parcels.component.html',
-  styleUrls: ['./parcels.component.css']
-})
-export class ParcelsComponent {
-}
